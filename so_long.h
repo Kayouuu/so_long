23ffe3559c99e9ed926c6687afee004a5facfcee
Pierre Saulnier <saulnier254@gmail.com>
2021-12-22 18:11:54 +0100

    map show

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: psaulnie <psaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 09:27:19 by psaulnie          #+#    #+#             */
-/*   Updated: 2021/12/21 14:38:36 by psaulnie         ###   ########.fr       */
+/*   Updated: 2021/12/22 17:20:21 by psaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,37 @@
 # include <stdio.h>
 # include <fcntl.h>
 
+# define SPRITE0 0
+# define SPRITE1 1
+# define SPRITE2 2
+# define SPRITE3 3
+# define SPRITE4 4
+# define SPRITE5 5
+# define SPRITE6 6
+# define SPRITE7 7
+# define CHEST 8
+# define GRASS 9
+# define TREE 10
+# define COIN 11
+
 typedef enum e_type
 {
 	WALL,
 	COLLECTIBLE,
 	SPACE,
-	PLAYER
+	PLAYER,
+	EXIT
 }				t_type;
 
 typedef struct s_size {
 	int	width;
 	int	height;
 }				t_size;
+
+typedef struct s_mlx {
+	void	*mlx;
+	void	*mlx_win;
+}				t_mlx;
 
 typedef struct s_pos {
 	int	x;
@@ -52,5 +71,10 @@ typedef struct s_img {
 /*	PARSING.C	*/
 
 char	**parsing(char *map);
+
+/*	SPRITES.C	*/
+
+void	draw_map(t_mlx mlx, char **sprites, char **map);
+char	**get_sprites(void *mlx);
 
 #endif
